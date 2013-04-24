369cdf9e4b9379abdee2520be58d0bf1a5e90475
Patrick Friedel <pfriedel@gmail.com>
2013-04-24 17:13:08 -0500

    Functional, but opaque.

--- TinyFiveISR.ino
+++ TinyFiveISR.ino
@@ -139,12 +139,12 @@ uint16_t b;
 uint8_t led;
 uint8_t max_brite = 255>>DEPTH;
 
-void draw_frame(void){
+ISR(TIMER0_COMPA_vect) {
   // giving the loop a bit of breathing room seems to prevent the last LED from flickering.  Probably optimizes into oblivion anyway.
   for ( led=0; led<15; led++ ) { 
     // software PWM 
     // input range is 0 (off) to 255>>DEPTH (127/63/31/etc) (fully on)
-
+    
     // Light the LED in proportion to the value in the led_grid array
     for( b=0; b < led_grid[led]; b++ ) {
       DDRB = led_dir[led];
@@ -214,65 +214,6 @@ void SleepNow(void) {
   sleep_disable(); // disable sleep mode for safety
 }
 
-void AllRand(void) {
-  uint8_t allrand_time = 5;
-  // Clear the display so it doesn't have any cruft left over from the prior mode.
-  while(1) {
-    for(int x = 0; x<=15; x++) {
-      led_grid[x] = 0;
-    }
-    
-    switch(random(14)) {
-      // red modes
-    case 0: 
-      HueWalk(allrand_time,millis(),20,1); // wide virtual space, slow progression
-      break;
-    case 1:
-      HueWalk(allrand_time,millis(),20,2); // wide virtual space, medium progression
-      break;
-    case 2:
-      HueWalk(allrand_time,millis(),20,5); // wide virtual space, fast progression
-      break;
-    case 3:
-      HueWalk(allrand_time,millis(),5,1); // 1:1 space to LED, slow progression
-      break;
-    case 4:
-      HueWalk(allrand_time,millis(),5,2); // 1:1 space to LED, fast progression
-      break;
-      // green modes
-    case 5: 
-      RandomColorRandomPosition(allrand_time,millis());
-      break;
-    case 6:
-      SBWalk(allrand_time,millis(),1,1); // Slow progression through hues modifying brightness
-      break; 
-    case 7:
-      SBWalk(allrand_time,millis(),4,1); // fast progression through hues modifying brightness
-      break;
-    case 8:
-      RandHueWalk(allrand_time,millis());
-      break;
-      // blue modes
-    case 9:
-      BiColorWalk(allrand_time, millis(), 0, 120); // red to green, works great
-      break;
-    case 10:
-      BiColorWalk(allrand_time, millis(), 120, 240); // green to blue, works great
-      break;
-    case 11:
-      BiColorWalk(allrand_time, millis(), 240, 359); // blue to red, works great - setting endhue to 0 makes it a very wiiiide walk.
-      break;
-    case 12:
-      {
-	uint16_t starthue = random(360);
-	uint16_t endhue = starthue + 120;
-	BiColorWalk(allrand_time, millis(), starthue, endhue);
-	break;
-      }
-    }
-  }
-}
-
 void setup() {
   if(bit_is_set(MCUSR, PORF)) { // Power was just established!
     MCUSR = 0; // clear MCUSR
@@ -295,12 +236,28 @@ void setup() {
     seed = (seed<<1) | (analogRead(1)&1);
   }
   randomSeed(seed);
+
+  OCR0A = 128;
+  /*
+CS bits:
+02 01 00
+0  0  0 = Clock stopped
+0  0  1 = No prescaling
+0  1  0 = /8
+0  1  1 = /64
+1  0  0 = /256
+1  0  1 = /1024
+  */
+
+  TCCR0B = _BV(CS01) | _BV(CS00); // /64 = dim but good speed 
+  TIMSK |= _BV(OCIE0A);
+  sei();
 }
 
 void loop() {
   // indicate which mode we're entering
   led_grid[last_mode] = 255>>DEPTH;
-  draw_for_time(1000);
+  delay(1000);
   led_grid[last_mode] = 0;
   delay(250);
 
@@ -308,74 +265,8 @@ void loop() {
   EESaveSettings();
 
   // go into the modes
-  switch(last_mode) {
-    // red modes
-  case 0: 
-    HueWalk(run_time,millis(),20,1); // wide virtual space, slow progression
-    break;
-  case 1:
-    HueWalk(run_time,millis(),20,2); // wide virtual space, medium progression
-    break;
-  case 2:
-    HueWalk(run_time,millis(),20,5); // wide virtual space, fast progression
-    break;
-  case 3:
-    HueWalk(run_time,millis(),5,1); // 1:1 space to LED, slow progression
-    break;
-  case 4:
-    HueWalk(run_time,millis(),5,2); // 1:1 space to LED, fast progression
-    break;
-    // green modes
-  case 5: 
-    RandomColorRandomPosition(run_time,millis());
-    break;
-  case 6:
-    RandHueWalk(run_time,millis()); // It's a lot like the prior mode, but with color shifting
-    break;
-  case 7:
-    SBWalk(run_time,millis(),1,1); // Slow progression through hues modifying brightness
-    break; 
-  case 8:
-    SBWalk(run_time,millis(),4,1); // fast progression through hues modifying brightness
-    break;
-  case 9:
-    PrimaryColors(run_time,millis()); // just a bulb check, to be honest.
-    break;
-    // blue modes
-  case 10:
-    BiColorWalk(run_time, millis(), 0, 120); // red to green, works great
-    break;
-  case 11:
-    BiColorWalk(run_time, millis(), 120, 240); // green to blue, works great
-    break;
-  case 12:
-    BiColorWalk(run_time, millis(), 240, 359); // blue to red, works great - setting endhue to 0 makes it a very wiiiide walk.
-    break;
-  case 13:
-    {
-      uint16_t starthue = random(360);
-      uint16_t endhue = starthue + 120;
-      BiColorWalk(run_time, millis(), starthue, endhue);
-      break;
-    }
-  case 14:
-    AllRand();
-    break;
-  }
+  HueWalk(run_time,millis(),20,1); // wide virtual space, slow progression
   SleepNow();
-}
-  
-void RandomColorRandomPosition(uint16_t time, uint32_t start_time) {
-  // preload all the LEDs with a color
-  for(int x = 0; x<5; x++) {
-    setLedColorHSV(x, random(MAX_HUE), 255, 255);
-  }
-  // and start blinking new ones in once a second.
-  while(1) {
-    setLedColorHSV(random(5), random(MAX_HUE), 255, 255);
-    draw_for_time(1000);
-    if(millis() >= (start_time + (time * time_multiplier))) { break; }
-  }
 }
 
 void HueWalk(uint16_t time, uint32_t start_time, uint8_t width, uint8_t speed) {
@@ -390,131 +281,14 @@ void HueWalk(uint16_t time, uint32_t start_time, uint8_t width, uint8_t speed) {
 	uint16_t hue = ((led) * MAX_HUE/(width)+colorshift)%MAX_HUE;
 	setLedColorHSV(led, hue, 255, 255);
       }
-      draw_for_time(DRAW_TIME);
+      delay(10);
     }
   }
 }
 
 /*
-time: How long it should run
-jump: How much should hue increment every time an LED flips direction?
-mode: 
-  1 = walk through brightnesses at full saturation, 
-  2 = walk through saturations at full brightness.
-1 tends towards colors & black, 2 tends towards colors & white.
-note: mode 2 is kind of funny looking with the current HSV->RGB code.  
-*/
-void SBWalk(uint16_t time, uint32_t start_time, uint8_t jump, uint8_t mode) {
-  uint8_t scale_max, delta;
-  uint16_t hue = random(MAX_HUE); // initial color
-  uint8_t led_val[5] = {37,29,21,13,5}; // some initial distances
-  bool led_dir[5] = {1,1,1,1,1}; // everything is initially going towards higher brightnesses
-
-  scale_max = 254; 
-  delta = 2; 
-
-  while(1) {
-    if(millis() >= (start_time + (time * time_multiplier))) { break; }
-    for(uint8_t led = 0; led<5; led++) {
-      if(mode == 1)       { setLedColorHSV(led, hue, 254,          led_val[led]);  } 
-      else if (mode == 2) { setLedColorHSV(led, hue, led_val[led], 254); }
-      
-      // if the current value for the current LED is about to exceed the top or the bottom, invert that LED's direction
-      if((led_val[led] >= (scale_max-delta)) or (led_val[led] <= (0+delta))) {
-	led_dir[led] = !led_dir[led];
-	if(led_val[led] <= (0+delta)) {
-	  hue += jump;
-	}
-	if(hue >= MAX_HUE)
-	  hue = 0;
-      }
-      if(led_dir[led] == 1)
-        led_val[led] += delta;
-      else 
-        led_val[led] -= delta;
-    }
-    draw_for_time(10);
-  }
-}
-
-void PrimaryColors(uint16_t time, uint32_t start_time) {
-  uint8_t led_bright = 1;
-  bool led_dir = 1;
-  uint8_t led = 0;
-  while(1) {
-    if(millis() >= start_time + (time*time_multiplier)) { break; }
-    
-    // flip the direction when the LED is at full brightness or no brightness.
-    if((led_bright >= (255>>DEPTH)) or (led_bright <= 0))
-      led_dir = !led_dir;
-    
-    // increment or decrement the brightness
-    if(led_dir == 1)
-      led_bright++;
-    else
-      led_bright--;
-    
-    // if the decrement will turn off the current LED, switch to the next LED
-    if( led_bright <= 0 ) { 
-      led_grid[led] = 0; 
-      led++; 
-    }
-    
-    // And if that change pushes the current LED off the end of the spire, reset to the first LED.
-    if( led >=15) 
-      led = 0; 
-
-    // push the change out to the array.
-    led_grid[led] = led_bright;
-    draw_for_time(2*(DEPTH+1));
-  }
-}
-
-// rotate 5 random color distances around the ring. Works quite nicely.  Leans heavily on randomseed being random.
-void RandHueWalk(uint16_t time, uint32_t start_time) {
-  uint16_t ledhue[] = {random(360), random(360), random(360), random(360), random(360)};
-  while(1) {
-    
-    if(millis() >= (start_time + (time * time_multiplier))) { break; }
-    
-    for(uint8_t led = 0; led<5; led++) {
-      
-      uint16_t hue = ledhue[led]--;
-      
-      if(hue == 0) { ledhue[led] = 359; }
-      setLedColorHSV(led, hue, 255, 255);
-    }
-    draw_for_time(DRAW_TIME); // this gets called 5 times, once per LED.
-  }
-}
-
-/* Additional ideas:
-1) Consider hitting endhue, then desaturating to white and ending up at the starthue.
-2) Maybe put all 5 LEDs on slightly different hues within the same start/endhue progression, then larson the colors back and forth?
-*/
-
-void BiColorWalk(uint16_t time, uint32_t start_time, uint16_t starthue, uint16_t endhue) {
-  uint16_t curhue = starthue;
-  int8_t curdir = 1;
-  while(1) {
-    if(millis() >= (start_time + (time * time_multiplier))) { break; }
-
-    curhue += curdir;
-    if(curhue == endhue) { curdir = -1; }
-    if(curhue == starthue) { curdir = 1; }
-
-    for(uint8_t led = 0; led < 5; led++) {
-      setLedColorHSV(led, curhue, 255, 255);
-    }
-    draw_for_time(DRAW_TIME * 3);
-  }
-}
-
-
-/*
-Inputs:
+  Inputs:
   p : LED to set
-  immediate : Whether the change should go to led_grid or led_grid_next
   hue : 0-359 - color
   sat : 0-255 - how saturated should it be? 0=white, 255=full color
   val : 0-255 - how bright should it be? 0=off, 255=full bright
@@ -579,10 +353,5 @@ void setLedColorHSV(uint8_t p, int16_t hue, int16_t sat, int16_t val) {
   led_grid[p] = r>>DEPTH;
   led_grid[p+5] = g>>DEPTH;
   led_grid[p+10] = b>>DEPTH;
-}
-
-// runs draw_frame a supplied number of times.
-void draw_for_time(uint16_t time) {
-  for(uint16_t f = 0; f<time * (1 << (DEPTH-1)); f++) { draw_frame(); }
 }
 
